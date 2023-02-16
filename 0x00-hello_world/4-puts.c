098367592de5132a17ea246a1dd8ebf37330129a
oluwaschunne <falope.oluwaseun2@gmail.com>
2023-02-16 13:55:55 -0800

    Betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point,
+ * to print Programming is like building a multilingual puzzle
+ * ends with a new line
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
