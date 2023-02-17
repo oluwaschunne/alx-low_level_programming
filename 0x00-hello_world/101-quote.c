201dbe11b5ca73e4485b746b9383eed889e8fb60
oluwaschunne <falope.oluwaseun2@gmail.com>
2023-02-16 18:57:23 -0800

    Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - a C program that prints
- * 
- * Return: Return 1.
+ * main - program that prints the size of various types of bytes
+ * Return: Always return 0.
 */
-
 int main(void)
 {
-	write(2,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-	return (1);
+	return (0);
 }
