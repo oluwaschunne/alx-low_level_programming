3fccf9b05942983401e8747e759d9fb7db74fa9e
oluwaschunne <falope.oluwaseun2@gmail.com>
2023-04-10 10:01:17 -0700

    Bit Manipulating

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -14,19 +14,16 @@ unsigned int binary_to_uint(const char *b)
 	j = 0;
 	if (!b)
 		return (0);
-
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		j <<= 1;
 		if (b[i] == '1')
 			j += 1;
 	}
-
 	return (j);
 }
