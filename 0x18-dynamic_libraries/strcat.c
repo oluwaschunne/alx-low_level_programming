28e1ae4be9f33e4b46f386da65ef04e62f55ee22
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-06-20 10:22:43 -0700

    dynamic libraries

--- 0x18-dynamic_libraries/strcat.c
+++ 0x18-dynamic_libraries/strcat.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * _strncat - Concatenates two strings using at most
- *            an inputted number of bytes from src.
- * @dest: The string to be appended upon.
- * @src: The string to be appended to dest.
- * @n: The number of bytes from src to be appended to dest.
+ * strcat - Concatenates the string pointed to by @src, including the terminating
+ *          null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
  *
- * Return: A pointer to the resulting string dest.
+ * Return: A pointer to the destination string @dest.
  */
-char *_strncat(char *dest, char *src, int n)
+char *strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
--- 0x18-dynamic_libraries/strcpy.c
+++ 0x18-dynamic_libraries/strcpy.c
@@ -1,25 +1,23 @@
 #include "main.h"
+
 /**
- * _strncpy - Copies at most an inputted number
- *            of bytes from string src into dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- * @n: The maximum number of bytes to copied from src.
- *
- * Return: A pointer to the resulting string dest.
+ * _strcpy - a function that copies the string pointed to by src
+ * including the terminating null byte (\0),
+ * o the buffer pointed to by dest.
+ * @dest: A pointer to destination of string
+ * @src: A pointer to source string to copy
+ * Return: the pointer to dest
  */
-char *_strncpy(char *dest, char *src, int n)
+
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0, src_len = 0;
+	int a, b = 0;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
+	for (a = 0; src[a] != '\0'; ++a)
+	{	dest[b] = src[a];
+	++b;
+	}
+	dest[b] = '\0';
 
 	return (dest);
 }
